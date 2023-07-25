b3bfece6ae408f77f2856fe85e521f1f3ecee7b0
Rahul R <rahulreddy23feb@gmail.com>
2023-07-26 00:12:51 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 
 let pScore = 0 , cScore = 0;
+let playChoice = 'rock'
 
 function getComputerChoice() {
-    let options = ['Rock','Paper','Scissors']
+    let options = ['rock','paper','scissors']
     return options[Math.floor(Math.random() * 3)] // you made a mistake here
 }
 
 function playRound(playerSelection, computerSelection) {
     let result = ""
-    let pSelected = playerSelection.toLowerCase();
-    let cSelected = computerSelection.toLowerCase()??'rock';
+    let pSelected = playerSelection
+    let cSelected = computerSelection
     if(pSelected === cSelected){
         result = "Its a draw"
         return result;
@@ -43,27 +44,59 @@ function playRound(playerSelection, computerSelection) {
             }
             break;
     }
-    return result
-
+    const pscore = document.querySelector('#pscore')
+    const cscore = document.querySelector('#cscore')
+    const message = document.querySelector('.message')
+    const finalResult = document.querySelector('.final-result')
+    pscore.innerHTML = `Player Score = ${pScore}`
+    cscore.innerHTML = `Computer Score = ${cScore}`
+    message.innerHTML = result
+    if (pScore == 5) {
+        finalResult.innerHTML = "Game over , You Win !"
+        resetScores()
+    }
+    else if (cScore == 5) {
+        finalResult.innerHTML = "Game over , You Lose!"
+        resetScores()
+    }else {
+        finalResult.innerHTML = "Result Pending ..."
+    }
   }
 
-function game() {
-    for(let i = 0; i<5;i++){
-        const playerSelection = prompt('Enter rock/paper/scissors')
-        const computerSelection = getComputerChoice();
-        console.log(playerSelection)
-        console.log(computerSelection)
-        console.log(playRound(playerSelection, computerSelection));
-
-    }
-    if(pScore>cScore){
-        console.log("You Win!")
-    }else if(cScore>pScore){
-        console.log("Computer wins")
-    }else{
-        console.log("Its a Draw")
-    }
+  function resetScores() {
+    pScore = 0;
+    cScore = 0;
+    const pscore = document.querySelector('#pscore');
+    const cscore = document.querySelector('#cscore');
+    pscore.innerHTML = `Player Score = ${pScore}`;
+    cscore.innerHTML = `Computer Score = ${cScore}`;
     
-}
-   
-game();
+  }
+
+  function game() {
+    const rock = document.querySelector('#r');
+    rock.addEventListener('click', () => {
+      const playerSelection = 'rock';
+      const computerSelection = getComputerChoice();
+      playRound(playerSelection, computerSelection); 
+    });
+  
+    const paper = document.querySelector('#p');
+    paper.addEventListener('click', () => {
+      const playerSelection = 'paper';
+      const computerSelection = getComputerChoice();
+      playRound(playerSelection, computerSelection); 
+    });
+  
+    const scissors = document.querySelector('#s');
+    scissors.addEventListener('click', () => {
+      const playerSelection = 'scissors';
+      const computerSelection = getComputerChoice();
+      playRound(playerSelection, computerSelection); 
+    });
+  }
+
+
+
+game()
+
